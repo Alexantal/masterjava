5209ecfa39b26c74d5d12b0faf16f343e8c2a89d
alexey_melnikov <alexantal@yandex.ru>
2021-11-19 13:26:17 +0300

    user_group table in init

--- config_templates/sql/initDB.sql
+++ config_templates/sql/initDB.sql
@@ -1,11 +1,12 @@
-DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS user_groups;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS cities;
 DROP TABLE IF EXISTS project_groups;
 DROP TABLE IF EXISTS projects;
 DROP SEQUENCE IF EXISTS city_seq;
 DROP SEQUENCE IF EXISTS user_seq;
 DROP SEQUENCE IF EXISTS project_seq;
-DROP SEQUENCE IF EXISTS project_group_seq;
+DROP SEQUENCE IF EXISTS group_seq;
 
 DROP TYPE IF EXISTS user_flag;
 DROP TYPE IF EXISTS group_type;
@@ -16,10 +17,10 @@ CREATE TYPE group_type AS ENUM ('registering', 'current', 'finished');
 CREATE SEQUENCE city_seq START 100000;
 CREATE SEQUENCE user_seq START 100000;
 CREATE SEQUENCE project_seq START 100000;
-CREATE SEQUENCE project_group_seq START 100000;
+CREATE SEQUENCE group_seq START 100000;
 
 CREATE TABLE cities (
-    id          INTEGER PRIMARY KEY DEFAULT nextval(city_seq),
+    id          INTEGER PRIMARY KEY DEFAULT nextval('city_seq'),
     city_code   TEXT NOT NULL,
     value       TEXT NOT NULL,
     CONSTRAINT city_code_value_idx UNIQUE (city_code, value)
@@ -37,7 +38,7 @@ CREATE TABLE users (
 CREATE UNIQUE INDEX email_idx ON users (email);
 
 CREATE TABLE  projects (
-    id              INTEGER PRIMARY KEY DEFAULT nextval(project_seq),
+    id              INTEGER PRIMARY KEY DEFAULT nextval('project_seq'),
     project_name    TEXT NOT NULL,
     description     TEXT NOT NULL
 );
@@ -45,6 +46,7 @@ CREATE TABLE  projects (
 CREATE UNIQUE INDEX project_name_idx ON projects (project_name);
 
 CREATE TABLE project_groups (
+    id              INTEGER PRIMARY KEY DEFAULT nextval('group_seq'),
     project_id      INTEGER NOT NULL,
     group_name      TEXT NOT NULL,
     type            group_type NOT NULL,
@@ -52,3 +54,11 @@ CREATE TABLE project_groups (
 );
 
 CREATE UNIQUE INDEX group_name_idx ON project_groups (group_name);
+
+CREATE TABLE user_groups (
+    user_id     INTEGER NOT NULL,
+    group_id    INTEGER NOT NULL,
+    CONSTRAINT user_group_idx UNIQUE (user_id, group_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (group_id) REFERENCES project_groups(id)
+)
